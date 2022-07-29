5718f25f5f0060f4b754f926fc6f345b6b801d08
Langchao Wu <langchaowu@yahoo.com>
2022-07-28 22:23:40 -0400

    import emailJs for contact section and add responsive layout

--- src/App.js
+++ src/App.js
@@ -7,23 +7,50 @@ import gameshop from './gameshop.png';
 import goldpanning from './goldpanning.png';
 import github from './github.png';
 import pokedex from './pokedex.png';
+import logo from './logo.png';
+import linkedIn from './linkedin.png';
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowsLeftRight, faSchool ,faGraduationCap, faBriefcase} from '@fortawesome/free-solid-svg-icons'
-import {useState} from 'react';
+import { faArrowsLeftRight, faSchool ,faGraduationCap, faBriefcase, faBars} from '@fortawesome/free-solid-svg-icons'
+import {useState,useRef} from 'react';
 import React from 'react';
+import emailjs from '@emailjs/browser';
+
 
 function App() {
+  const form = useRef();
   const [isWork,setIsWork]=useState(false);
   function timeLineChange(){
     setIsWork(!isWork);
   }
 
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_8vhd1lh', 'template_zu6m8s8', form.current, '8tKDJCkxIXF77dj2T')
+      .then((result) => {
+          // console.log(result.text);
+          alert('email sent');
+      }, (error) => {
+          // console.log(error.text);
+          alert(`${error.text}, please try later`);
+      });
+      e.target.reset();
+  };
+
   return (
     <div className="App">
-        
+      <div className='logo'><img src={logo}/></div>
       <div className='spline-container'id='home'>
+        <div className='menu'>
+            <FontAwesomeIcon icon={faBars}/>
+            <a href="#home" className='menu-text'>Home</a>
+            <a href="#about" className='menu-text'>About</a>
+            <a href="#project" className='menu-text'>Projects</a>
+            <a href="#contact" className='menu-text'>Contact</a>
+        </div>
           <div className='nav-bar'>
               <a href="#home" className='nav-text'>Home</a>
               <a href="#about" className='nav-text'>About</a>
@@ -35,7 +62,10 @@ function App() {
           <div className='Welcome-text'>
               <div className='welcome-text-1'>Welcome to my portfolio!</div>
               <div className='welcome-text-2'> Hi, I'm Langchao Wu   </div>
-
+              <a href='https://www.linkedin.com/in/langchao-wu/' target="_blank" className='connect'>
+                <img className='linkedin-logo' src={linkedIn}/>
+                <div>Let's connect!</div>
+                </a>
               <div className='welcome-text-2'>
                 <Typical 
                 loop={Infinity}
@@ -73,7 +103,7 @@ function App() {
            <div className='two-button'>
             
               <div className='education_active'>
-                <FontAwesomeIcon className='education-icon' icon={faGraduationCap} />
+                <FontAwesomeIcon className='education-icon_active' icon={faGraduationCap} />
                 <span>Education</span>
                 </div>
 
@@ -92,7 +122,7 @@ function App() {
                 </div>
 
               <div className='work_active'>
-                <FontAwesomeIcon icon={faBriefcase} />
+                <FontAwesomeIcon className='work-icon_active' icon={faBriefcase} />
                 <span>Work</span>
               </div>
 
@@ -105,10 +135,10 @@ function App() {
                 <VerticalTimeline>
                   <VerticalTimelineElement
                     className="vertical-timeline-element--work"
-                    contentStyle={{ background: 'rgb(118, 89, 141)', color: '#fff' }}
+                    contentStyle={{ background: 'rgb(178, 128, 243)', color: '#fff' }}
                     contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
                     date="2015 - 2019"
-                    iconStyle={{ background: 'rgb(118, 89, 141)', color: '#b8ccddb8ccdd' }}
+                    iconStyle={{ background: 'rgb(178, 128, 243)', color: '#b8ccddb8ccdd' }}
                     icon={<FontAwesomeIcon icon={faArrowsLeftRight} />}
                   >
                     <h2 className="vertical-timeline-element-title">Computer Science</h2>
@@ -121,10 +151,10 @@ function App() {
 
                   <VerticalTimelineElement
                     className="vertical-timeline-element--work"
-                    contentStyle={{ background: 'rgb(118, 89, 141)', color: '#fff' }}
+                    contentStyle={{ background: 'rgb(178, 128, 243)', color: '#fff' }}
                     contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
                     date="2022-2 - 2022-6"
-                    iconStyle={{ background: 'rgb(118, 89, 141)', color: '#fff' }}
+                    iconStyle={{ background: 'rgb(178, 128, 243)', color: '#fff' }}
                     icon={<FontAwesomeIcon icon={faArrowsLeftRight} />}
                   >
                     <h2 className="vertical-timeline-element-title">Software Engineer</h2>
@@ -139,10 +169,10 @@ function App() {
           <VerticalTimeline>
             <VerticalTimelineElement
               className="vertical-timeline-element--work"
-              contentStyle={{ background: 'rgb(118, 89, 141)', color: '#fff' }}
+              contentStyle={{ background: 'rgb(178, 128, 243)', color: '#fff' }}
               contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
               date="2018 - 2019"
-              iconStyle={{ background: 'rgb(118, 89, 141)', color: '#fff' }}
+              iconStyle={{ background: 'rgb(178, 128, 243)', color: '#fff' }}
               icon={<FontAwesomeIcon icon={faArrowsLeftRight} />}
             >
               <h2 className="vertical-timeline-element-title">Sales Associate</h2>
@@ -155,10 +185,10 @@ function App() {
 
             <VerticalTimelineElement
               className="vertical-timeline-element--work"
-              contentStyle={{ background: 'rgb(118, 89, 141)', color: '#fff' }}
+              contentStyle={{ background: 'rgb(178, 128, 243)', color: '#fff' }}
               contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
               date="2020 - 2022"
-              iconStyle={{ background: 'rgb(118, 89, 141)', color: '#fff' }}
+              iconStyle={{ background: 'rgb(178, 128, 243)', color: '#fff' }}
               icon={<FontAwesomeIcon icon={faArrowsLeftRight} />}
             >
               <h2 className="vertical-timeline-element-title">Customer Service Representative</h2>
@@ -227,18 +257,18 @@ function App() {
       
       <div id='contact' className='contact-container'>
           <h1>Contact me</h1>
-          <form>
+          <form ref={form} onSubmit={sendEmail}>
             <div className='form-content'>
               <div className='input-field'>
-                <input type="text" className='form-name' placeholder='Name'/>
-                <input type="text" className='form-email' placeholder='Email Address'/>
+                <input type="text" className='form-name' name='name' placeholder='Name'/>
+                <input type="text" className='form-email' name='email' placeholder='Email Address'/>
               </div>
               
               <div className='input-field'>
-               <input type="text" className='form-subject' placeholder='Subject'/>
+               <input type="text" className='form-subject'name='subject' placeholder='Subject'/>
               </div>
               <div className='input-field'>
-                <textarea className='form-message' placeholder='Message'/>
+                <textarea className='form-message' name="message" placeholder='Message'/>
               </div>
               <div className='input-field'>
                <input type="submit" className='form-submit' value='Send Message'/>
